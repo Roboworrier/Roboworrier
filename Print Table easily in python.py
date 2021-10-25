6368ce001eb26bbeacd69b710ff3f9bb2f5c13b9
Diwakar Singh <91152334+Roboworrier@users.noreply.github.com>
2021-10-25 23:51:57 +0530

    Add files via upload
    Write Table of any Integer Number with  this one line code .

---
+++ Print Table easily in python.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+#for i in range(0, ax10 + [1] , a)     where a = the number you want to print table of .
+#print(i)
+#example : i wanna print tableof 7 so {7x10 + 1} =71 hence range(0, 71,7)
+for i in range(0, 71 , 7):
+    print(i)
+
+
+# # Let's write a table in easy steps.
+# #command
+# for i in range(0 ,( [The number you want to print table of] x 10) + (1) , The number you want to print table of):
+# print(i)
+# #Example:
+# if i wanna print table of 9 then i'll enter 
+# for i in range(0,91 , 9):      { 9 x 10 = 90  , 90 + 1= 91}
+# print(i)..
+# #you can write table of any number easily.
+# #Thanks
+
+# In[10]:
+
+
+pwd
+
+
+# In[ ]:
+
+
+
+
